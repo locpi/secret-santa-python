67e8efff9739f3ccba46af291a2445a3b298a24d
Loïc Pinçon <loic.pincon29@gmail.com>
2024-03-03 20:04:46 -0500

    edit setup.cy

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-import os
 from setuptools import setup
 
 setup(
     name = "secret santa",
     version = "0.0.1",
-    author = "Loïc Pinço",
-    license = "GNU",
+    author = "Loïc Pinçno",
     packages=['secretsanta', 'tests'],
 )
